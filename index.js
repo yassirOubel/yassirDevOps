773e9f54199dede4ac804f97fcc107a10f02dc0e
yassir <157588435+yassirOubel@users.noreply.github.com>
2024-11-06 12:32:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/plain');
-res.end('Negralato non e il mio presidente');
+res.end('Maiala Harrys non e il mio presidente');
 });
 server.listen(port, hostname, () => {
 console.log(`Server running at http://${hostname}:${port}/`);
